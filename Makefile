294f1876fbcd81ec767e6cf71d01036681478ff7
Pablo Moreira De Sousa <pmoreira@c1r1s9.42lisboa.com>
2024-10-29 16:00:17 +0000

    strchr

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ NAME = libft.a
 C_SOURCE = ft_bzero.c ft_atoi.c ft_calloc.c ft_isalnum.c ft_isalpha.c \
 ft_isascii.c ft_isdigit.c ft_isprint.c ft_memchr.c ft_memcmp.c \
 ft_memcpy.c ft_memset.c ft_strlen.c ft_strncmp.c ft_tolower.c ft_toupper.c \
-ft_strlcpy.c ft_strlcat.c
+ft_strlcpy.c ft_strlcat.c ft_strchr.c
 
 H_SOURCE = ./
 
---
+++ ft_strchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 15:30:13 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/10/29 15:44:21 by pmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c);
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (0);
+}
