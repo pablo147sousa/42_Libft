2b9ed4a6b11583d853e29b7d610817b1a6ba2900
Pablo Moreira De Sousa <pmoreira@c1r1s9.42lisboa.com>
2024-10-29 15:29:29 +0000

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:43:56 by pmoreira          #+#    #+#             */
-/*   Updated: 2024/10/29 14:50:45 by pmoreira         ###   ########.fr       */
+/*   Updated: 2024/10/29 15:25:43 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,20 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	j;
+	size_t	sizeT;
 
+	if (size <= 0)
+		return (0);
 	i = ft_strlen(dst);
-	while (i < size)
+	j = 0;
+	sizeT = ft_strlen(dst) + ft_strlen(src);
+	while (src[j] != 0 && i < size - 1)
 	{
-		
+		dst[i] = src[j];
+		j++;
+		i++;
 	}
-	
+	dst[i] = 0;
+	return (sizeT);
 }
