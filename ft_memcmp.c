a4fbf2c60d38734001047e8595cdef19bbe385fb
Pablo Moreira De Sousa <pmoreira@c1r1s9.42lisboa.com>
2024-10-30 14:36:36 +0000

    Fix memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 14:47:01 by pmoreira          #+#    #+#             */
-/*   Updated: 2024/10/28 16:05:37 by pmoreira         ###   ########.fr       */
+/*   Updated: 2024/10/30 14:34:29 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,9 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = (unsigned char *) s1;
 	str2 = (unsigned char *) s2;
 	i = 0;
-	while ((str1[i] == str2[i]) && str1[i] != 0 && i < (n - 1))
+	if (n == 0)
+		return (0);
+	while ((str1[i] == str2[i]) && i < (n - 1))
 		i++;
 	return (str1[i] - str2[i]);
 }
