47128cf53cf7fb6b27fec69e0c55908d947e23ab
Pablo Moreira De Sousa <pmoreira@c1r1s9.42lisboa.com>
2024-10-30 14:54:19 +0000

    Fix strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:30:13 by pmoreira          #+#    #+#             */
-/*   Updated: 2024/10/29 16:32:07 by pmoreira         ###   ########.fr       */
+/*   Updated: 2024/10/30 14:52:31 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ char	*ft_strchr(const char *s, int c);
 
 char	*ft_strchr(const char *s, int c)
 {
-	int		i;
-	char	*str;
+	int				i;
+	char			*str;
+	unsigned char	letter;
 
 	str = (char *) s;
+	letter = (unsigned char) c;
 	i = 0;
 	while (str[i] != 0)
 	{
-		if (str[i] == c)
+		if (str[i] == letter)
 			return (&str[i]);
 		i++;
 	}
+	if (str[i] == letter)
+		return (&str[i]);
 	return (0);
 }
