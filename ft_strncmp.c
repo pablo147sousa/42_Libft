9a6a24ec55bf32668eabb6598e5eb393328f47de
Pablo Moreira De Sousa <pmoreira@c1r1s9.42lisboa.com>
2024-10-30 15:05:19 +0000

    Fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:09:22 by pmoreira          #+#    #+#             */
-/*   Updated: 2024/10/28 12:53:42 by pmoreira         ###   ########.fr       */
+/*   Updated: 2024/10/30 15:00:09 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (n == 0)
+		return (0);
 	while ((s1[i] == s2[i]) && s1[i] != 0 && i < (n - 1))
 		i++;
 	return (s1[i] - s2[i]);
