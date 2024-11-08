d7e7e219b5be08ee2129cc5547b02b2c12e1732f
Pablo Moreira De Sousa <pmoreira@c1r1s11.42lisboa.com>
2024-11-08 14:49:22 +0000

    lstdelone

---
+++ ft_lstdelone.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 14:29:43 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/11/08 14:49:01 by pmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	del(lst->content);
+	free(lst);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:59:03 by pmoreira          #+#    #+#             */
-/*   Updated: 2024/11/08 12:58:59 by pmoreira         ###   ########.fr       */
+/*   Updated: 2024/11/08 14:44:53 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,6 +101,8 @@ int		ft_lstsize(t_list *lst); //ok
 
 void	ft_lstadd_front(t_list **lst, t_list *new); //ok
 
-void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new); //ok
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
